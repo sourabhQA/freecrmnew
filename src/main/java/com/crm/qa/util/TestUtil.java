5c65dd46f079729dad60d354571ebbaba9a3bd65
sourabhQA <sourabhsignh66@gmail.com>
2018-08-27 17:09:06 +0530

    last push

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -14,6 +14,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
 
 import com.crm.qa.base.TestBase;
 
@@ -31,7 +32,7 @@ public class TestUtil extends TestBase{
 	
 		String currentdir = System.getProperty("user.dir");
 		TakesScreenshot scrShot =((TakesScreenshot)Driver);
-	
+
 		File scrFile = 	scrShot.getScreenshotAs(OutputType.FILE); 
 		try {
 			FileUtils.copyDirectory(scrFile, new File(currentdir + "/SCREENSHOTS/" + System.currentTimeMillis() + ".png"));
